47b97273e2fd132a762faac7cd06847ffe38618d
Henrique Esteves <henriesteves@gmail.com>
2017-07-28 20:54:48 -0300

    - Listagem de Category
    - Criação das Rotas de Category

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h3>Listagem de Categorias</h3>
+            <a href="{{ route('category.create') }}" class="btn btn-primary">Nova Categoria</a>
+        </div>
+        
+        <div class="row">
+
+            <table class="table table-striped">
+
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    @foreach($categories as $category)
+                    <tr>
+                        <td>{{ $category->id }}</td>
+                        <td>{{ $category->name }}</td>
+                        <td>
+                            Actions
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+
+            </table>
+
+        </div>
+    </div>
+@endsection
